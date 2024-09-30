4d1b7e8040f6c04a0b562736410406287998a7d6
Payman <payman.hoseini7@gmail.com>
2024-09-30 21:20:23 +0330

    responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -99,11 +99,11 @@ export default function Home() {
   return (
     <>
       <main className={`${theme} bg-main_background h-screen w-screen grid place-content-center`}>
-        <div className="max-w-md w-[960px] mx-auto">
+        <div className="max-w-md md:w-[960px] w-[90vw] mx-auto">
             <div className="flex items-end justify-between">
                 <h1 className={`${theme == "theme_one" ? "text-text-two" : "text-text"} font-LeagueSpartan text-3xl`}>calc</h1>
                 <div className="flex items-end space-x-5">
-                  <p className={`${theme == "theme_one" ? "text-text-two" : "text-text"} font-LeagueSpartan text-xs`}>THEME</p>
+                  <p className={`${theme == "theme_one" ? "text-text-two" : "text-text"} font-LeagueSpartan text-xs tracking-widest`}>THEME</p>
                   <div>
                     <div className={`${theme == "theme_one" ? "text-text-two" : "text-text"} font-LeagueSpartan text-sm space-x-4 px-2`}>
                       <span>1</span>
@@ -111,27 +111,27 @@ export default function Home() {
                       <span>3</span>
                     </div>
                     <div className="switch-toggle space-x-1 switch-3 switch-candy">
-                      <input id="one" name="theme" type="radio" onClick={setThemeHandler} checked={(theme == "theme_one" ? true : false)}/>
+                      <input id="one" name="theme" type="radio" onClick={setThemeHandler} checked={(theme == "theme_one" ? true : false)} onChange={()=>{}}/>
                       <label htmlFor="on"></label>
 
-                      <input id="two" name="theme" type="radio" onClick={setThemeHandler} checked={(theme == "theme_two" ? true : false)}/>
+                      <input id="two" name="theme" type="radio" onClick={setThemeHandler} checked={(theme == "theme_two" ? true : false)} onChange={()=>{}}/>
                       <label htmlFor="na" ></label>
 
-                      <input id="three" name="theme" type="radio" onClick={setThemeHandler} checked={(theme == "theme_three" ? true : false)}/>
+                      <input id="three" name="theme" type="radio" onClick={setThemeHandler} checked={(theme == "theme_three" ? true : false)} onChange={()=>{}}/>
                       <label htmlFor="off"></label>
                     </div>
                   </div>
                 </div>
             </div>
-            <div className="bg-screen_background mt-6 rounded-lg flex items-center py-8 px-7 justify-end">
-              <p className={`${theme == "theme_one" ? "text-text-two" : "text-text"} font-LeagueSpartan text-5xl right-7`}>{calc.num ? calc.num : calc.res}</p>
+            <div className="bg-screen_background mt-6 rounded-lg flex items-center md:py-8 py-5 px-7 justify-end">
+              <p className={`${theme == "theme_one" ? "text-text-two" : "text-text"} font-LeagueSpartan text-5xl overflow-hidden`}>{calc.num ? calc.num : calc.res}</p>
             </div>
-            <div className="grid grid-cols-4 gap-5 bg-keypad_background p-7 mt-5 rounded-lg">
+            <div className="grid grid-cols-4 md:gap-5 gap-3 bg-keypad_background md:p-7 p-5 mt-5 rounded-lg">
               {
                 btnValues.map((btn , i) => {
                   return (
-                    <Button key={i} className={(btn == "DEL") ? "font-LeagueSpartan text-2xl text-text-two bg-key_background rounded-lg border-b-4 border-b-key_shadow flex justify-center items-center  hover:brightness-125 hover:cursor-pointer" : 
-                      (btn == "RESET") ? "font-LeagueSpartan text-2xl text-text-two bg-key_background rounded-lg border-b-4 border-b-key_shadow flex justify-center items-center col-span-2  hover:brightness-125 hover:cursor-pointer" :
+                    <Button key={i} className={(btn == "DEL") ? "font-LeagueSpartan md:text-2xl text-xl text-text-two bg-key_background rounded-lg border-b-4 border-b-key_shadow flex justify-center items-center  hover:brightness-125 hover:cursor-pointer" : 
+                      (btn == "RESET") ? "font-LeagueSpartan md:text-2xl text-xl text-text-two bg-key_background rounded-lg border-b-4 border-b-key_shadow flex justify-center items-center col-span-2  hover:brightness-125 hover:cursor-pointer" :
                       (btn == "=") ? "font-LeagueSpartan text-[32px] text-text-two bg-key-equal-btn rounded-lg border-b-4 border-b-key-equal-btn-shadow flex justify-center col-span-2  hover:brightness-125 hover:cursor-pointer" :
                       "font-LeagueSpartan text-[32px] text-text bg-key-active-back rounded-lg border-b-4 border-b-key-active-shadow flex justify-center  hover:brightness-[1.15] hover:cursor-pointer"} value={btn} 
                       onClick={
